b513c49d1cf41e4f54f357820d33d649a0f2f68e
muhammadichsanulkahfi <muhammadichsanulkahfi@gmail.com>
2025-05-06 18:53:30 +0700

    comit 1

---
+++ CONTOH1.cpp
@@ -0,0 +1,53 @@
+//Nama Repository : PointerdanFriendClass_#NIM
+#include <iostream>
+using namespace std;
+
+class PersegiPanjang
+{
+public: // akses modifier
+    int panjang, lebar;
+
+    public:
+        void input()
+        {
+            cout << "Masukkan panjang : ";
+            cin >> panjang;
+            cout << "Masukkan lebar : ";
+            cin >> lebar;
+
+        }
+
+        int luasPp(int a, int b)
+        {
+            return a * b;
+        }
+        //Lingkaran oL;
+};
+
+class Lingkaran 
+{
+public:
+    int jarijari;
+
+    void input()
+    {
+        cout << "Masukkan jari-jari : ";
+        cin >> jarijari;
+    }
+
+    int luasLingkaran(int r)
+    {
+        return 3.14 * r * r;
+    }
+};
+
+int main()
+{
+    PersegiPanjang objekPP; //membuat objek
+    Lingkaran ol;
+
+    cout << "Masukkan panjang : ";
+    cin >> objekPP.panjang;
+    cout << "Masukkan Lebar : ";
+    cin >> objekPP.Lebar;
+}
